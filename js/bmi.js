d5d8354c9ecb112d4ecf1c71ac9aa5894af62d2c
jerry <jerry@gmail.com>
2022-06-17 21:47:16 +0800

    新增bulma計算

--- js/bmi.js
+++ js/bmi.js
@@ -1,27 +1,36 @@
-const nametEl = document.querySelector("#name");
 const weightEl = document.querySelector("#weight");
 const heightEl = document.querySelector("#height");
 const calcEl = document.querySelector("#calc");
 const bmiEl = document.querySelector("#result .bmi");
 
-console.log(nametEl, weightEl, heightEl, calcEl);
-
 calcEl.addEventListener("click", () => {
-    let name = nametEl.value;
     let weight = weightEl.value;
     let height = heightEl.value;
 
-
     let bmi = getBmi(height, weight);
+
     if (bmi == Infinity || isNaN(bmi)) {
         bmiEl.innerText = "輸入錯誤";
-
         return;
     }
 
-    // alert(`${name} bmi=${bmi}`);
-    console.log(bmi);
     bmiEl.innerText = bmi;
+    bmiEl.classList.remove("has-text-danger");
+    bmiEl.classList.remove("has-text-success");
+    let comment = "";
+    if (bmi < 18.5) {
+        comment = "體重過輕!";
+    } else if (bmi >= 18.5 && bmi < 24) {
+        bmiEl.classList.add("has-text-success");
+        comment = "標準身材!";
+    } else if (bmi >= 24 && bmi < 27) {
+        bmiEl.classList.add("has-text-danger");
+        comment = "有點過重囉!";
+    } else {
+        bmiEl.classList.add("has-text-danger");
+        comment = "真的胖!要開始減肥囉!";
+    }
+    document.querySelector("#result .comment").innerText = comment;
 });
 
 
